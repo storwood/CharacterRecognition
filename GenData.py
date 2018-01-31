d3d2aea48e2a0e8d559af6583bf095be61466149
Kamil <kamilkawa1994@gmail.com>
2018-01-31 20:35:17 +0100

    Added TC

--- GenData.py
+++ GenData.py
@@ -50,7 +50,7 @@ def main():
     intValidChars = [ord('0'), ord('1'), ord('2'), ord('3'), ord('4'), ord('5'), ord('6'), ord('7'), ord('8'), ord('9'),
                      ord('a'), ord('b'), ord('c'), ord('d'), ord('e'), ord('f'), ord('g'), ord('h'), ord('i'), ord('j'),
                      ord('k'), ord('l'), ord('m'), ord('n'), ord('o'), ord('p'), ord('q'), ord('r'), ord('s'), ord('t'),
-                     ord('u'), ord('v'), ord('w'), ord('x'), ord('y'), ord('z')]
+                     ord('u'), ord('v'), ord('w'), ord('x'), ord('y'), ord('z'), ord(','), ord('.'), ord('?'), ord('!')]
 
     for npaContour in npaContours:                          # for each contour
         if cv2.contourArea(npaContour) > MIN_CONTOUR_AREA:          # if contour is big enough to consider
